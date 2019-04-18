1c86f249f66817ff708eda050fb048d1d23e0cc3
COMP202_Qinglong_Wang <qinglong.wang@mail.mcgill.ca>
2019-04-18 12:21:56 -0400

    server bash file

--- run_SL_server.bash
+++ run_SL_server.bash
@@ -1,3 +1,51 @@
+touch log/SL2/100k/uni_t_ep500_b100_h10.log
+touch log/SL2/100k/o2_t_ep500_b100_h10.log
+touch log/SL2/100k/m_ep500_b100_h10.log
+touch log/SL2/100k/mi_ep500_b100_h10.log
+touch log/SL2/100k/srn_t_ep500_b100_h10.log
+touch log/SL2/100k/lstm_ep500_b100_h10.log
+touch log/SL2/100k/gru_ep500_b100_h10.log
+
+touch log/SL4/100k/uni_t_ep500_b100_h10.log
+touch log/SL4/100k/o2_t_ep500_b100_h10.log
+touch log/SL4/100k/m_ep500_b100_h10.log
+touch log/SL4/100k/mi_ep500_b100_h10.log
+touch log/SL4/100k/srn_t_ep500_b100_h10.log
+touch log/SL4/100k/lstm_ep500_b100_h10.log
+touch log/SL4/100k/gru_ep500_b100_h10.log
+
+touch log/SL8/100k/uni_t_ep500_b100_h10.log
+touch log/SL8/100k/o2_t_ep500_b100_h10.log
+touch log/SL8/100k/m_ep500_b100_h10.log
+touch log/SL8/100k/mi_ep500_b100_h10.log
+touch log/SL8/100k/srn_t_ep500_b100_h10.log
+touch log/SL8/100k/lstm_ep500_b100_h10.log
+touch log/SL8/100k/gru_ep500_b100_h10.log
+
+touch log/SP2/100k/uni_t_ep500_b100_h10.log
+touch log/SP2/100k/o2_t_ep500_b100_h10.log
+touch log/SP2/100k/m_ep500_b100_h10.log
+touch log/SP2/100k/mi_ep500_b100_h10.log
+touch log/SP2/100k/srn_t_ep500_b100_h10.log
+touch log/SP2/100k/lstm_ep500_b100_h10.log
+touch log/SP2/100k/gru_ep500_b100_h10.log
+
+touch log/SP4/100k/uni_t_ep500_b100_h10.log
+touch log/SP4/100k/o2_t_ep500_b100_h10.log
+touch log/SP4/100k/m_ep500_b100_h10.log
+touch log/SP4/100k/mi_ep500_b100_h10.log
+touch log/SP4/100k/srn_t_ep500_b100_h10.log
+touch log/SP4/100k/lstm_ep500_b100_h10.log
+touch log/SP4/100k/gru_ep500_b100_h10.log
+
+touch log/SP8/100k/uni_t_ep500_b100_h10.log
+touch log/SP8/100k/o2_t_ep500_b100_h10.log
+touch log/SP8/100k/m_ep500_b100_h10.log
+touch log/SP8/100k/mi_ep500_b100_h10.log
+touch log/SP8/100k/srn_t_ep500_b100_h10.log
+touch log/SP8/100k/lstm_ep500_b100_h10.log
+touch log/SP8/100k/gru_ep500_b100_h10.log
+
 python main.py --data 'SL/SL2/100k/' --epoch 500 --batch 100 --test_batch 10 --rnn UNI --act tanh --nhid 10 --seed 1 > log/SL2/100k/uni_t_ep500_b100_h10.log
 python main.py --data 'SL/SL2/100k/' --epoch 500 --batch 100 --test_batch 10 --rnn O2 --act tanh --nhid 10 --seed 1 > log/SL2/100k/o2_t_ep500_b100_h10.log
 python main.py --data 'SL/SL2/100k/' --epoch 500 --batch 100 --test_batch 10 --rnn M --nhid 10 --seed 1 > log/SL2/100k/m_ep500_b100_h10.log
