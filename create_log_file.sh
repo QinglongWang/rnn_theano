dc9cb9f75787df7662b0b9fc190ab1e7305228e8
COMP202_Qinglong_Wang <qinglong.wang@mail.mcgill.ca>
2019-04-18 12:23:58 -0400

    server bash file

---
+++ create_log_file.sh
@@ -0,0 +1,47 @@
+touch log/SL2/100k/uni_t_ep500_b100_h10.log
+touch log/SL2/100k/o2_t_ep500_b100_h10.log
+touch log/SL2/100k/m_ep500_b100_h10.log
+touch log/SL2/100k/mi_ep500_b100_h10.log
+touch log/SL2/100k/srn_t_ep500_b100_h10.log
+touch log/SL2/100k/lstm_ep500_b100_h10.log
+touch log/SL2/100k/gru_ep500_b100_h10.log
+
+touch log/SL4/100k/uni_t_ep500_b100_h10.log
+touch log/SL4/100k/o2_t_ep500_b100_h10.log
+touch log/SL4/100k/m_ep500_b100_h10.log
+touch log/SL4/100k/mi_ep500_b100_h10.log
+touch log/SL4/100k/srn_t_ep500_b100_h10.log
+touch log/SL4/100k/lstm_ep500_b100_h10.log
+touch log/SL4/100k/gru_ep500_b100_h10.log
+
+touch log/SL8/100k/uni_t_ep500_b100_h10.log
+touch log/SL8/100k/o2_t_ep500_b100_h10.log
+touch log/SL8/100k/m_ep500_b100_h10.log
+touch log/SL8/100k/mi_ep500_b100_h10.log
+touch log/SL8/100k/srn_t_ep500_b100_h10.log
+touch log/SL8/100k/lstm_ep500_b100_h10.log
+touch log/SL8/100k/gru_ep500_b100_h10.log
+
+touch log/SP2/100k/uni_t_ep500_b100_h10.log
+touch log/SP2/100k/o2_t_ep500_b100_h10.log
+touch log/SP2/100k/m_ep500_b100_h10.log
+touch log/SP2/100k/mi_ep500_b100_h10.log
+touch log/SP2/100k/srn_t_ep500_b100_h10.log
+touch log/SP2/100k/lstm_ep500_b100_h10.log
+touch log/SP2/100k/gru_ep500_b100_h10.log
+
+touch log/SP4/100k/uni_t_ep500_b100_h10.log
+touch log/SP4/100k/o2_t_ep500_b100_h10.log
+touch log/SP4/100k/m_ep500_b100_h10.log
+touch log/SP4/100k/mi_ep500_b100_h10.log
+touch log/SP4/100k/srn_t_ep500_b100_h10.log
+touch log/SP4/100k/lstm_ep500_b100_h10.log
+touch log/SP4/100k/gru_ep500_b100_h10.log
+
+touch log/SP8/100k/uni_t_ep500_b100_h10.log
+touch log/SP8/100k/o2_t_ep500_b100_h10.log
+touch log/SP8/100k/m_ep500_b100_h10.log
+touch log/SP8/100k/mi_ep500_b100_h10.log
+touch log/SP8/100k/srn_t_ep500_b100_h10.log
+touch log/SP8/100k/lstm_ep500_b100_h10.log
+touch log/SP8/100k/gru_ep500_b100_h10.log
